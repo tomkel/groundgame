25d5ce26b57cd4acf1e0cbc10bb21095a9ea5818
Tommy Kelly <git@tkel.ly>
2019-07-01 14:47:43 -0700

    Got a working drawer from the right with app bar on top

---
+++ src/components/hamburger.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import clsx from 'clsx'
+import PropTypes from 'prop-types'
+import './hamburger.scss'
+
+const Hamburger = ({ open }) => {
+  return (
+    <button className={clsx('hamburger hamburger--arrowalt-r', open && 'is-active')} type="button">
+      <span className="hamburger-box">
+        <span className="hamburger-inner" />
+      </span>
+    </button>
+  )
+}
+
+Hamburger.propTypes = {
+  open: PropTypes.bool.isRequired,
+}
+
+export default Hamburger
