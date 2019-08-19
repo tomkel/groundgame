1fae6ed006955a7e0e552ea3caadf330f17ac2a9
Tommy Kelly <git@tkel.ly>
2019-08-19 14:16:24 -0700

    Rename index2 to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { MuiThemeProvider } from '@material-ui/core/styles'
-import Layout from '../components/layout2'
-import theme from '../theme'
+import Layout from './components/layout2'
+import theme from './theme'
 
-export default function Index2() {
+export default function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <Layout />
